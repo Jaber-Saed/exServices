c9082ebe68bde994735b38bf1dfa0e001d47b16a
Jaber-Saed <92320333+Jaber-Saed@users.noreply.github.com>
2023-08-30 10:58:41 +0300

    new exServses

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AngularComponent } from './angular/angular.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AngularComponent
   ],
   imports: [
     BrowserModule
